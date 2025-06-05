347876f92df5d3ba5005989fd88a188e3c2a4122
Gopika Bhardwaj <gopikab@sdfiana006.sdf.slac.stanford.edu>
2025-06-05 14:57:50 -0700

    Continuously running every minute

--- k2eg/k2eg_mlflow.py
+++ k2eg/k2eg_mlflow.py
@@ -4,6 +4,8 @@ import torch
 import logging
 import json
 import math
+import time
+import datetime
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -43,92 +45,80 @@ next_run_number = max(run_numbers, default=0) + 1
 run_name = f"Live K2EG LUME Model Run{next_run_number}"
 logger.info(f"Starting new MLflow run: {run_name}")
 
+# Load transformers and model
+logger.info("Loading transformers and model components...")
+input_variables, output_variables = variables_from_yaml("../model/pv_variables.yml")
+lume_module = TorchModule("../model/pv_module.yml")
 
-with mlflow.start_run(run_name=run_name):
+# Read live input from K2EG
+logger.info("Reading input PVs from live EPICS data using K2EG...")
+k2eg_client = k2eg.dml('lcls', 'app-three')
+pv_names = lume_module.model.input_names
 
-    # Load transformers and model
-    logger.info("Loading transformers and model components...")
-    input_variables, output_variables = variables_from_yaml("../model/pv_variables.yml")
-    lume_module = TorchModule("../model/pv_module.yml")
-
-    # Read live input from K2EG
-    logger.info("Reading input PVs from live EPICS data using K2EG...")
-    k2eg_client = k2eg.dml('lcls', 'app-three')
-    pv_map_path = os.path.join("..", "info", "pv_mapping.json")
-    pv_names = lume_module.model.input_names
-
-    input_parameter_values = {}
-    for pv_name in pv_names:
-        input_parameter_values[pv_name] = input_variables[pv_names.index(pv_name)].default_value
-        if pv_name not in ['CAMR:IN20:186:R_DIST', 'Pulse_length'] and not input_variables[pv_names.index(pv_name)].is_constant:
-            try:
-                input_parameter_values[pv_name] = k2eg_client.get('ca://' + pv_name, 5.0)["value"]
-            except Exception as e:
-                logger.warning(f"Failed to get PV {pv_name}: {e}. Using default.")
-
+with mlflow.start_run(run_name=run_name) as parent_run:
+    logger.info("Started parent run")
     try:
-        in_xrms_value = k2eg_client.get('ca://CAMR:IN20:186:XRMS')["value"]
-        in_yrms_value = k2eg_client.get('ca://CAMR:IN20:186:YRMS')["value"]
-        rdist = math.sqrt(in_xrms_value ** 2 + in_yrms_value ** 2)
-        input_parameter_values['CAMR:IN20:186:R_DIST'] = rdist
-    except Exception as e:
-        logger.error(f"Failed to compute R_DIST: {e}. Using default.")
+        while True:
+            with mlflow.start_run(nested=True):
+                timestamp_str = datetime.datetime.now().strftime("%m-%d-%Y %H:%M:%S")
+                mlflow.set_tag("readable_timestamp", timestamp_str)
+                input_parameter_values = {}
+                for pv_name in pv_names:
+                    input_parameter_values[pv_name] = input_variables[pv_names.index(pv_name)].default_value
+                    if pv_name not in ['CAMR:IN20:186:R_DIST', 'Pulse_length'] and not input_variables[pv_names.index(pv_name)].is_constant:
+                        try:
+                            input_parameter_values[pv_name] = k2eg_client.get('ca://' + pv_name, 5.0)["value"]
+                        except Exception as e:
+                            logger.warning(f"Failed to get PV {pv_name}: {e}. Using default.")
 
-    for pv_name, val in input_parameter_values.items():
-        logger.info(f"Input Dict: {pv_name} → {val}")
+                try:
+                    in_xrms_value = k2eg_client.get('ca://CAMR:IN20:186:XRMS')["value"]
+                    in_yrms_value = k2eg_client.get('ca://CAMR:IN20:186:YRMS')["value"]
+                    rdist = math.sqrt(in_xrms_value ** 2 + in_yrms_value ** 2)
+                    input_parameter_values['CAMR:IN20:186:R_DIST'] = rdist
+                except Exception as e:
+                    logger.error(f"Failed to compute R_DIST: {e}. Using default.")
 
-    with open("input_parameters.txt", "w") as f:
-        for pv_name, val in input_parameter_values.items():
-            f.write(f"{pv_name} → {val}\n")
+                for pv_name, val in input_parameter_values.items():
+                    logger.info(f"Input Dict: {pv_name} → {val}")
 
-    mlflow.log_artifact("input_parameters.txt")
-    os.remove("input_parameters.txt")
+                with open("input_parameters.txt", "w") as f:
+                    for pv_name, val in input_parameter_values.items():
+                        f.write(f"{pv_name} → {val}\n")
 
-    # Predict
-    input_tensor = torch.tensor(list(input_parameter_values.values())).to(
-        dtype=lume_module.model.dtype,
-        device=lume_module.model.device
-    ).unsqueeze(0)
+                mlflow.log_artifact("input_parameters.txt")
+                os.remove("input_parameters.txt")
 
-    with torch.no_grad():
-        predictions = lume_module(input_tensor)
+                # Predict
+                input_tensor = torch.tensor(list(input_parameter_values.values())).to(
+                    dtype=lume_module.model.dtype,
+                    device=lume_module.model.device
+                ).unsqueeze(0)
 
-    logger.info(f"Predictions: {predictions}")
+                with torch.no_grad():
+                    predictions = lume_module(input_tensor)
 
+                logger.info(f"Predictions: {predictions}")
 
-    # Log predictions 
+                
+                # Log predictions 
+                current_time_s = int(time.time())
+                for i, name in enumerate(lume_module.model.output_names):
+                    clean_name = name.replace(":", "_")
+                    value = predictions[i].item() if isinstance(predictions[i], torch.Tensor) else predictions[i]
+                    mlflow.log_metric(clean_name, value, timestamp=current_time_s)
 
-    """lume_module.register_to_mlflow(
-            input_tensor,
-            artifact_path="lcls_injector_torch_module",
-            registered_model_name="lcls_injector_torch_module", # not necessary but required for adding tags/aliases
-            tags={"type":"surrogate_injector"}, # example tag, if desired
-            run_name=run_name)"""
-    # Plot predictions
-    logger.info("Plotting predictions...")
-    nrows, ncols = 3, 2
-    fig, ax = plt.subplots(nrows=nrows, ncols=ncols, figsize=(10, 15))
-    for i, output_name in enumerate(lume_module.output_order):
-        ax_i = ax[i // ncols, i % ncols]
-        ax_i.plot(predictions[i].detach().numpy(), "C1x", label="predictions")
-        ax_i.legend()
-        ax_i.set_title(output_name)
-    ax[-1, -1].axis('off')
-    fig.tight_layout()
+            time.sleep(60)
 
-    fig_name = f"{run_name.replace(' ', '_').lower()}_prediction_plot.png"
-    mlflow.log_figure(fig, fig_name)
-    logging.info(f"Logged figure: {fig_name}")
-    plt.close()
-    logging.info("Saved and logged plot")
+    except KeyboardInterrupt:
+        logger.info("Interrupted by user. Shutting down...")
 
-    # Log model to registry
-    
-
-    k2eg_client.close()
-    logger.info("Inference complete and logged to MLflow.")
+    finally:
+        k2eg_client.close()
+        logger.info("Inference complete and logged to MLflow.")
 
 # End run if still active
 if mlflow.active_run() is not None:
     mlflow.end_run()
     logger.info("MLflow run ended.")
+
